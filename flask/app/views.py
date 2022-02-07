099ce6cbd54686ab0314d4fa4c603686de112333
burakuren101 <burakuren101@gmail.com>
2022-02-07 21:08:18 +0300

    added response headers

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,10 +1,15 @@
 from app import app
+import flask
 from flask import render_template
 
 @app.route("/")
 def hello():
-    return "<h1>Hello From Flask!</h1>"
+    response = flask.Response("<h1>Hello From Flask!</h1>")
+    response.headers["Server"] = "WebServer"
+    return response
 
 @app.route("/movie")
 def home():
-    return render_template("home.html")
+    response = flask.Response(render_template("home.html"))
+    response.headers["Server"] = "WebServer"
+    return response
