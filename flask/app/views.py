8d7ad8ed376e0cc411201ca8a4c30665ffd7398e
burakuren101 <burakuren101@gmail.com>
2022-02-07 20:54:36 +0300

    first html file servedd

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,7 +1,10 @@
 from app import app
-
+from flask import render_template
 
 @app.route("/")
-def home():
+def hello():
     return "<h1>Hello From Flask!</h1>"
 
+@app.route("/movie")
+def home():
+    return render_template("home.html")
