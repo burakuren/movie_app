47a904e501d6c85c5b977f3734a15ddabe604504
burakuren101 <burakuren101@gmail.com>
2022-02-08 23:44:02 +0300

    a image serverd my another method

--- flask/app/views.py
+++ flask/app/views.py
@@ -2,7 +2,7 @@ from crypt import methods
 from datetime import date
 from app import app
 import flask
-from flask import render_template,request
+from flask import render_template,request,send_file,make_response
 
 @app.route("/")
 def hello():
@@ -16,9 +16,15 @@ def home():
     response.headers["Server"] = "WebServer"
     return response
 
+@app.route("/static/gonca.jpg")
+def gonca_jpeg():
+    response = flask.make_response(send_file("static/gonca.jpg",mimetype='image/jpeg'))
+    response.headers["Server"] = "WebServer"
+    return response
+
 @app.route("/emosea")
 def emosea():
-    response = flask.Response(render_template("emosea.html"))
+    response = flask.make_response(render_template("emosea.html"))
     response.headers["Server"] = "WebServer"
     return response
 
