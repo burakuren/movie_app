1949e8a4cfbdcdab205d4d0df6199c4c734d055b
burakuren101 <burakuren101@gmail.com>
2022-02-09 14:45:04 +0300

    file name fixed

--- flask/app/views.py
+++ flask/app/views.py
@@ -5,14 +5,14 @@ import flask
 from flask import render_template,request,send_file,make_response
 
 @app.route("/")
-def hello():
+def home():
     response = flask.Response("<h1>Hello From Flask!</h1>")
     response.headers["Server"] = "WebServer"
     return response
 
 @app.route("/movie")
-def home():
-    response = flask.Response(render_template("home.html"))
+def movie():
+    response = flask.Response(render_template("movie.html"))
     response.headers["Server"] = "WebServer"
     return response
 
@@ -27,4 +27,6 @@ def emosea():
     response = flask.make_response(render_template("emosea.html"))
     response.headers["Server"] = "WebServer"
     return response
+
+
 
